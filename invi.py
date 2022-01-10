d51ae1ffc4453aaa978bbf9c1c32cf8bf60a7ad1
Mr. Foisal <83734733+MohammadFoisal@users.noreply.github.com>
2022-01-10 10:35:32 +0600

    Update invi.py

--- invi.py
+++ invi.py
@@ -1,11 +1,12 @@
-
- 
-
-import base64, codecs
-magic = 'aW1wb3J0IHJlcXVlc3RzCmltcG9ydCBqc29uCm1zaXNkbj1pbnB1dCgiXG5cdGVudGVyIE51bWJlciB3aXRoIDg4OiAiKQpuYW1lID0gaW5wdXQoIlxuRW50ZXIgTGtleTogIikKaT1pbnQoaW5wdXQoIkVudGVyIENvdW50OiAiKSkKeT0wCndoaWx'
-love = 'yVUx8nGbXVPNtVPNtqKWfZvN9VPqbqUEjpmbiY2AcpzAfMF5lo2WcYzAioF5vMP9grJkcMzHiLKOjLKOcY2SjpTAuoTjhpTujC29jCKAyozEQo250LJA0GTymqPMuL3Eco249nJ52nKEyWjbtVPNtVPOgrJ9vnwNtCFO7W21mnKAxovp6oKAcp2EhsD'
-god = 'ogICAgICB0cnk6CiAgICAgICAgeD1yZXF1ZXN0cy5wb3N0KHVybDIsIGRhdGEgPSBteW9iajAsIGhlYWRlcnMgPSB7IngtYXBpLWtleSI6IG5hbWUsIngtYXBwLWtleSI6IjAwMG9jMHNvNDhvd2t3NHMwd3dvNGMwMGcwMDgwNHc4MGd3a3c4a2cif'
-destiny = 'FxXVPNtVPNtVPO5CKxeZFNtVPNtVNbtVPNtVPNtVUOlnJ50XUxfrP50MKu0YPWpqUEupzqyqPOhqJ1vMKV6VPVfoKAcp2EhXFNtVNbtVPNtVPOyrTAypUD6VPNtVPNtVNbtVPNtVPNtVUOlnJ50XPWpoykhKUEpqRIlpz9lVTAioz5yozA0nJ9hVvx='
-joy = '\x72\x6f\x74\x31\x33'
-trust = eval('\x6d\x61\x67\x69\x63') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x6c\x6f\x76\x65\x2c\x20\x6a\x6f\x79\x29') + eval('\x67\x6f\x64') + eval('\x63\x6f\x64\x65\x63\x73\x2e\x64\x65\x63\x6f\x64\x65\x28\x64\x65\x73\x74\x69\x6e\x79\x2c\x20\x6a\x6f\x79\x29')
-eval(compile(base64.b64decode(eval('\x74\x72\x75\x73\x74')),'<string>','exec'))
+import requests, time
+print("Auto Error Blast!!!")
+Key = input("Enter Lkey: ")
+while 1:
+      url2 = 'https://circle.robi.com.bd/mylife/appapi/appcall.php?op=sendContactList&action=invite'
+      myobj0 = {'data':'[{ "msisdn": "8801819400400" }]'}
+      try:
+        x=requests.post(url2, data = myobj0, headers = {"x-api-key": Key,"x-app-key":"000oc0so48owkw4s0wwo4c00g00804w80gwkw8kg"})      
+        print(x.text)
+        time.sleep(2)
+      except:       
+        print("Server Error")
